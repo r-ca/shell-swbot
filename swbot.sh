7068c97698eaca1659df3ad1454ae269869a24aa
r-ca <nem@nem.x0.to>
2023-11-26 19:40:30 +0900

    操作できるようになった？

--- swbot.sh
+++ swbot.sh
@@ -37,15 +37,33 @@ if [ "${1}" = "setOn" -o "${1}" = "setOff" ]; then
 
     isSingle=true
     # デバイスIDのパース
+    DEVICE_COUNT=(${DEVICE_IDS//,/ }[0])
     DEVICE_ID_LIST=(${DEVICE_IDS//,/ })
-    if [ "${DEVICE_ID_LIST[0]}" -gt 1 ]; then
+    unset DEVICE_ID_LIST[0] # 0番目はデバイス数なので削除(なんとかしたい)
+    if [ "${DEVICE_COUNT}" -gt 1 ]; then
         isSingle=false
         ./logger "debug" "Multiple Device Mode" "SetMode"
     else 
         ./logger "debug" "Single Device Mode" "SetMode"
     fi
 
+    command=""
+    if [ "${1}" = "setOn" ]; then
+        command="turnOn"
+    else 
+        command="turnOff"
+    fi
 
+    for device_id in "${DEVICE_ID_LIST[@]}"; do 
+        ./logger "debug" "Execute device ID: ${device_id}" "SetMode"
+        resp=$(
+            curl -s -X POST \
+                -H "Authorization: ${TOKEN}" \
+                -H "Content-Type: application/json; charset=utf8" \
+                -d '{"command": "'${command}'", "parameter": "default", "commandType": "command"}' \
+                "https://api.switch-bot.com/v1.0/devices/${device_id}/commands"
+        )
+        ./logger "debug" "Response: ${resp}" "SetMode"
+    done
 
-    
 fi
