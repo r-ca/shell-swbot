e2adf922c94f051e783740b23a8b8494fbdded87
r-ca <nem@nem.x0.to>
2023-11-26 21:22:24 +0900

    wrapper: 呼び出し方とエントリーポイントを変更

--- wrapper.sh
+++ wrapper.sh
@@ -5,9 +5,9 @@
 ## SETTING
 TOKEN="token"
 DEVICE_ID_LIST=("device_id1" "device_id2")
-MODE="toggleOn"
+MODE="setOn"
 
 ## 変換
 DEVICE_IDS=${#DEVICE_ID_LIST[*]},$(IFS=,; echo "${DEVICE_ID_LIST[*]}")
 ## 実行
-TOKEN=$TOKEN DEVICE_IDS=$DEVICE_IDS ./swbot.sh $MODE
+TOKEN=$TOKEN DEVICE_IDS=$DEVICE_IDS ACTION=$MODE ./entry.sh
