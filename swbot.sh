24ec09d85ac87044bcbf13a834f7b9a3f212d5f1
r-ca <nem@nem.x0.to>
2023-11-26 23:04:07 +0900

    swbot:起動時にログを出力するようにした

--- swbot.sh
+++ swbot.sh
@@ -11,11 +11,18 @@
 # Logger
 LOGGER="$LOGGER_PATH swbot"
 
-$LOGGER "debug" "Entry Point Start"
-$LOGGER "debug" "Project Root: ${PROJECT_ROOT}"
-$LOGGER "debug" "Action Root: ${ACTION_ROOT}"
-$LOGGER "debug" "Common Root: ${COMMON_ROOT}"
-$LOGGER "debug" "Action: ${ACTION}"
+# Debug Log
+$LOGGER "debug" "Environment variables:"
+$LOGGER "debug" "  Path:"
+$LOGGER "debug" "    Project Root\t: ${PROJECT_ROOT}"
+$LOGGER "debug" "    Action Root\t: ${ACTION_ROOT}"
+$LOGGER "debug" "    Common Root\t: ${COMMON_ROOT}"
+$LOGGER "debug" "    Logger Path\t: ${LOGGER_PATH}"
+$LOGGER "debug" "  Env:"
+$LOGGER "debug" "    Token\t\t: ${TOKEN}"
+$LOGGER "debug" "    Device IDs\t\t: ${DEVICE_IDS}"
+$LOGGER "debug" "    Action\t\t: ${ACTION}"
+
 
 # Check Env
 checkEnv_result=$($PROJECT_ROOT"/common/check_env.sh")
