6c1087262db78892246832672fc0a4d451e904cb
r-ca <nem@nem.x0.to>
2023-11-26 21:29:59 +0900

    swbot: 外部スクリプトに切り出した

--- swbot.sh
+++ swbot.sh
@@ -6,63 +6,34 @@
 # warn: Warning
 # info: Information
 # succ: Success
+# debug: Debug
 
-# 設定値のチェック
-if [ -z "$TOKEN" ]; then
-    ./logger "err_c" "TOKEN is not set" "EnvCheck"
-    exit 1
-else 
-    ./logger "debug" "TOKEN : ${TOKEN}" "EnvCheck"
-fi
+# Logger
+LOGGER="$LOGGER_PATH swbot"
 
-if [ -z "$DEVICE_IDS" ]; then
-    ./logger "err_c" "DEVICE_IDS is not set" "EnvCheck"
-    exit 1
+$LOGGER "debug" "Entry Point Start"
+$LOGGER "debug" "Project Root: ${PROJECT_ROOT}"
+$LOGGER "debug" "Action Root: ${ACTION_ROOT}"
+$LOGGER "debug" "Common Root: ${COMMON_ROOT}"
+$LOGGER "debug" "Action: ${ACTION}"
+
+# Check Env
+checkEnv_result=$($PROJECT_ROOT"/common/check_env.sh")
+$LOGGER "debug" "CheckEnv result: ${checkEnv_result}"
+if [ "${checkEnv_result}" = "0" ]; then
+    $LOGGER "info" "Check environment variables successfully."
 else 
-    ./logger "debug" "DEVICE_IDS : ${DEVICE_IDS}" "EnvCheck"
+    checkEnv_failed_count=(${checkEnv_result//,/ }[0])
+    checkEnv_failed_list=(${checkEnv_result//,/ })
+    unset checkEnv_failed_list[0] # 0番目は失敗数なので削除(なんとかしたい)
+    $LOGGER "err_c" "Failed to check environment variables."
+    $LOGGER "err_c" "Failed count: ${checkEnv_failed_count}"
+    $LOGGER "err_c" "Failed list: ${checkEnv_failed_list[*]}"
+    exit 1
 fi
 
 # 操作タイプ分岐
-if [ "${1}" = "setOn" -o "${1}" = "setOff" ]; then
-    # ActionType: Set
-    ./logger "info" "ActionType: Set" "SetMode"
-    ./logger "info" "Mode: ${1}" "SetMode"
-
-    isSingle=true
-    # デバイスIDのパース
-    DEVICE_COUNT=(${DEVICE_IDS//,/ }[0])
-    DEVICE_ID_LIST=(${DEVICE_IDS//,/ })
-    unset DEVICE_ID_LIST[0] # 0番目はデバイス数なので削除(なんとかしたい)
-    if [ "${DEVICE_COUNT}" -gt 1 ]; then
-        isSingle=false
-        ./logger "info" "Multiple Device Mode" "SetMode"
-    else 
-        ./logger "info" "Single Device Mode" "SetMode"
-    fi
-
-    command=""
-    if [ "${1}" = "setOn" ]; then
-        command="turnOn"
-    else 
-        command="turnOff"
-    fi
-
-    for device_id in "${DEVICE_ID_LIST[@]}"; do 
-        ./logger "debug" "Execute device ID: ${device_id}" "SetMode"
-        resp=$(
-            curl -s -X POST \
-                -H "Authorization: ${TOKEN}" \
-                -H "Content-Type: application/json; charset=utf8" \
-                -d '{"command": "'${command}'", "parameter": "default", "commandType": "command"}' \
-                "https://api.switch-bot.com/v1.0/devices/${device_id}/commands"
-        )
-        ./logger "debug" "Response: ${resp}" "SetMode"
-        if [ $(echo $resp | jq ".statusCode") -ne 100 ]; then
-            ./logger "err" "Failed to execute." "SetMode"
-            exit 1
-        else 
-            ./logger "succ" "Successfully executed!" "SetMode"
-        fi
-    done
-
+if [ "${ACTION}" = "setOn" -o "${ACTION}" = "setOff" ]; then
+    $LOGGER "debug" "ActionType: Set"
+    $ACTION_ROOT"/SetMode.sh"
 fi
