3ea02aeed6a058bd42287b1cfab42db8d02a7123
r-ca <nem@nem.x0.to>
2023-11-26 21:30:21 +0900

    checkEnv: 切り出された2

---
+++ common/check_env.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+LOGGER=$PROJECT_ROOT"/common/logger.sh checkEnv"
+
+failed_count=0
+failed_list=()
+
+if [ -z "$TOKEN" ]; then
+    failed_count=$((failed_count+1))
+    failed_list+=("TOKEN")
+fi
+
+if [ -z "$DEVICE_IDS" ]; then
+    failed_count=$((failed_count+1))
+    failed_list+=("DEVICE_IDS")
+fi
+
+# 戻り値
+if [ $failed_count -gt 0 ]; then
+    echo ${failed_count}","$(IFS=,; echo "${failed_list[*]}")
+else 
+    echo "0"
+fi
