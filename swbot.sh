9fae416b6d65adfcc79fc1d8ee65df5aefa53193
r-ca <nem@nem.x0.to>
2023-11-26 19:17:48 +0900

    環境変数のチェック

--- swbot.sh
+++ swbot.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
 
+# Log Level
+# err_c: Critical Error
+# err: Error
+# warn: Warning
+# info: Information
+# succ: Success
 
-echo $TOKEN
+# 設定値のチェック
+if [ -z "$TOKEN" ]; then
+    ./logger "err_c" "TOKEN is not set" "EnvCheck"
+    exit 1
+else 
+    ./logger "debug" "TOKEN : ${TOKEN}" "EnvCheck"
+fi
+
+if [ -z "$SECRET" ]; then
+    ./logger "err_c" "SECRET is not set" "EnvCheck"
+    exit 1
+else 
+    ./logger "debug" "SECRET : ${SECRET}" "EnvCheck"
+fi
+
+if [ -z "$DEVICE_IDS" ]; then
+    ./logger "err_c" "DEVICE_IDS is not set" "EnvCheck"
+    exit 1
+else 
+    ./logger "debug" "DEVICE_IDS : ${DEVICE_IDS}" "EnvCheck"
+fi
+
+
