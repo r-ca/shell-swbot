bc509aa5213bc4634f3e5b9e03d7c7a2a174328c
r-ca <nem@nem.x0.to>
2023-11-26 21:29:07 +0900

    entry: EntryPointを新しく作った

---
+++ entry.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Entry Point
+
+# Init
+PROJECT_ROOT=$(pwd)
+ACTION_ROOT=$PROJECT_ROOT"/action"
+COMMON_ROOT=$PROJECT_ROOT"/common"
+
+# Logger
+LOGGER_PATH=$PROJECT_ROOT"/common/logger.sh"
+
+PROJECT_ROOT=${PROJECT_ROOT} \
+ACTION_ROOT=${ACTION_ROOT} \
+COMMON_ROOT=${COMMON_ROOT} \
+LOGGER_PATH=${LOGGER_PATH} \
+./swbot.sh
