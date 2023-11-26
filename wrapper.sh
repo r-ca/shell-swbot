a57294177412c2a9744a98e190a6005d09fdd995
r-ca <nem@nem.x0.to>
2023-11-26 18:54:25 +0900

    wrapper: 複数デバイスを扱えるようにした

--- wrapper.sh
+++ wrapper.sh
@@ -4,8 +4,11 @@
 
 TOKEN="token"
 SECRET="secret"
-DEVICE_ID="device_id"
+
+DEVICE_ID_LIST=("device_id1" "device_id2")
+
+DEVICE_IDS=${#DEVICE_ID_LIST[*]},$(IFS=,; echo "${DEVICE_ID_LIST[*]}")
 
 MODE="toggleOn"
 
-TOKEN=$TOKEN SECRET=$SECRET DEVICE_ID=$DEVICE_ID ./swbot.sh $MODE
+TOKEN=$TOKEN SECRET=$SECRET DEVICE_IDS=$DEVICE_IDS ./swbot.sh $MODE
